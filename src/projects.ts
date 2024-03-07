56280056ef8acd0b60038c8f9658ad38ff893a71
Mustanjid <mustanjid.se@gmail.com>
2024-03-07 12:40:29 +0600

    updated home page and award page

---
+++ src/projects.ts
@@ -0,0 +1,36 @@
+export const PROJECTS = [
+    {
+        title: 'Travo',
+        description: `A basic online blog where travellers can share their adventures. Adventures would be shared by stories including images, and places. One traveller can comment on other travellers' posts & rate them, as well as share. 
+        All of you need to do is just sign up for all the activities.`,
+        imgSrc: 'https://mustanjid.onrender.com/images/1.png',
+        href: 'https://github.com/mustanjid/Travo',
+        url: 'https://travo.onrender.com/',
+        stack: ['Node', 'Express', 'MongoDB'],
+    },
+    {
+        title: 'Github User Search',
+        description: `A simple app to search GitHub users with their username or email. Just put the username in the textbox, hit the search button and the result will be shown below.`,
+        imgSrc: 'https://github.com/mustanjid/githubProfileSearch/raw/main/public/promo.png',
+        href: 'https://github.com/mustanjid/githubProfileSearch',
+        url: 'https://githubusersearch.onrender.com/',
+        stack: ['Js', 'React', 'Github API'],
+    },
+    {
+        title: 'QuoteGen',
+        description: `This a simple random quote generator apps with a great user interface, also sharing options available for twitter only.`,
+        imgSrc: 'https://github.com/mustanjid/QuoteGen/raw/main/public/promogen.png',
+        href: 'https://github.com/mustanjid/githubProfileSearch',
+        url: 'https://randomquotegen.onrender.com/',
+        stack: ['Js', 'React', 'Quote API'],
+    },
+    {
+        title: 'Restful Routing with node',
+        description: `RESTFUL basic crud operation using node, express, mongoose, mongodb. Here mostly learn restful routing and how it connects with mongoose & save into mongodb.`,
+        imgSrc: 'https://cdn.pixabay.com/photo/2015/04/23/17/41/node-js-736399_1280.png',
+        href: 'https://github.com/Al-mustanjid/CRUD_WITH_EXPRESS_MONGO_RESTFUL',
+        url: '',
+        stack: ['Js', 'Express', 'MongoDB'],
+    },
+]
+
